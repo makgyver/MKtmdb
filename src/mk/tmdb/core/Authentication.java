b7406232d875f2d4616554e978e68710f6d6619f
Mirko <mak1788@gmail.com>
2012-12-20 18:00:14 +0100

    Added comments to the Authentication class.

--- src/mk/tmdb/core/Authentication.java
+++ src/mk/tmdb/core/Authentication.java
@@ -12,8 +12,23 @@ import mk.tmdb.utils.Log;
 import mk.tmdb.utils.ResponseObject;
 import mk.tmdb.utils.Status;
 
+/**
+ * This class provides methods to authenticate to the TMDB site.
+ * 
+ * @author Mirko Polato
+ *
+ */
 public class Authentication {
 
+	/**
+	 * This method is used to generate a valid request token for user based authentication. 
+	 * A request token is required in order to request a session id. 
+	 * You can generate any number of request tokens but they will expire after 60 minutes. 
+	 * As soon as a valid session id has been created the token will be destroyed.
+	 * 
+	 * @return The authentication token
+	 * @throws ResponseException Throws whether the server response is not a success.
+	 */
 	public static Token getNewToken() throws ResponseException {
 		
 		ResponseObject response = TMDBAPI.getAuthenticationToken();
@@ -25,6 +40,12 @@ public class Authentication {
 		}
 	}
 	
+	/**
+	 * Opens the request authorization web page in the default browser.
+	 * 
+	 * @param token The authentication token
+	 * @throws ResponseException Throws whether the server response is not a success.
+	 */
 	public static void showRequestAuthorization(Token token) throws ResponseException {
 		try {
 			
@@ -46,6 +67,14 @@ public class Authentication {
 		}
 	}
 	
+	/**
+	 * This method is used to generate a session id for user based authentication. 
+	 * A session id is required in order to use any of the write methods.
+	 * 
+	 * @param token The authentication token
+	 * @return The authenticated session ID
+	 * @throws ResponseException Throws whether the server response is not a success.
+	 */
 	public static String getNewSession(Token token) throws ResponseException {
 		
 		ResponseObject response = TMDBAPI.getAuthenticationSession(token);
@@ -57,6 +86,17 @@ public class Authentication {
 		}
 	}
 	
+	/**
+	 * This method is used to generate a guest session id. A guest session can be used to rate movies without 
+	 * having a registered TMDb user account. You should only generate a single guest session per user 
+	 * (or device) as you will be able to attach the ratings to a TMDb user account in the future. 
+	 * There is also IP limits in place so you should always make sure it's the end user doing the 
+	 * guest session actions. If a guest session is not used for the first time within 24 hours, 
+	 * it will be automatically discarded.
+	 * 
+	 * @return The authenticated session ID for a guest
+	 * @throws ResponseException Throws whether the server response is not a success.
+	 */
 	public static String getNewGuestSession() throws ResponseException {
 		
 		ResponseObject response = TMDBAPI.getAuthenticationGuestSession();
